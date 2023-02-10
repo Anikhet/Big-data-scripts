35e517be809e49ddcc73a257ffb216fcbd180326
Anikhet Mulky <51974582+Anikhet@users.noreply.github.com>
2023-02-09 19:58:42 -0500

    new addition

--- table.sql
+++ table.sql
@@ -18,16 +18,16 @@ CREATE TABLE person (
 
 COPY person FROM '/Users/anikhetmulky/Downloads/person.csv' WITH (FORMAT csv,null '\N');
 
-CREATE TABLE person(
+CREATE TABLE people(
     title_id int,
     name_id int,
-    PRIMARY KEY (title_id, name_id)
-    CONSTRAINT fk_title FOREIGN KEY (title_id) REFERENCES title (title_id) ON UPDATE CASCADE,
+    CONSTRAINT fk_title FOREIGN KEY (title_id) REFERENCES movie (title_id) ON UPDATE CASCADE,
     CONSTRAINT fk_name FOREIGN KEY (name_id) REFERENCES person (name_id) ON UPDATE CASCADE,
-    category VARCHAR(20)
+    role VARCHAR(20),
+    PRIMARY KEY (title_id, name_id,role)
     
 );
-COPY role FROM '/Users/anikhetmulky/Downloads/principals.csv' WITH (FORMAT csv,null '\N');
+COPY people FROM '/Users/anikhetmulky/Downloads/principals.csv' WITH (FORMAT csv,null '\N');
 
 CREATE TABLE ratevote(
     title_id int ,
